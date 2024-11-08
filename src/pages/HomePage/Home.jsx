186de2c8381bbcea075e97a5689ff8793799b91e
Fabricio Andrade <fabricioandradegt@gmail.com>
2024-11-08 15:57:29 -0300

    Criação dos componentes da página principal e iniciando a inteligência do carrinho de compras

---
+++ src/pages/HomePage/Home.jsx
@@ -0,0 +1,13 @@
+import MainSection from './MainSection';
+import CartOverlay from '../../components/Cart/CartOverlay';
+
+const Home = () => {
+  return (
+    <>
+      <CartOverlay />
+      <MainSection />
+    </>
+  );
+};
+
+export default Home;
